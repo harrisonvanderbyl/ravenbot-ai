d4566a318e429b64424717452da430074249d1e7
harrisonvanderbyl <harrison.vanderbyl@gmail.com>
2022-08-16 02:02:25 +1000

    stop

--- commands/slashCommands/webserver/express.ts
+++ commands/slashCommands/webserver/express.ts
@@ -16,38 +16,34 @@ console.log("Starting load of server");
 console.log("Exporting starts");
 
 export const start = async () => {
-  const privateKey1 = readFileSync(
-    "/etc/letsencrypt/live/" + config.redirectUrl + "/privkey.pem",
-    "utf8"
-  );
-  console.log(privateKey1);
-  const certificate1 = readFileSync(
-    "/etc/letsencrypt/live/" + config.redirectUrl + "/cert.pem",
-    "utf8"
-  );
-  const ca1 = readFileSync(
-    "/etc/letsencrypt/live/" + config.redirectUrl + "/chain.pem",
-    "utf8"
-  );
-  const credentials1 = {
-    key: privateKey1,
-    cert: certificate1,
-    ca: ca1,
-  };
-
-  console.log("Ending load of certs");
-
-  // Certificate for Domain 3
-  // ... (just like line 22-29)
-  // Certificate for Domain 4
-  // ... (just like line 22-29)
-
-  // Starting both http & https servers
-  //const httpServer = http.createServer(app);
-  const httpsServer = https.createServer(credentials1, app);
-  console.log("Starting server");
-
-  app.listen(443, () => {
-    console.log("HTTPS Server running on port 443");
-  });
+  //   const privateKey1 = readFileSync(
+  //     "/etc/letsencrypt/live/" + config.redirectUrl + "/privkey.pem",
+  //     "utf8"
+  //   );
+  //   console.log(privateKey1);
+  //   const certificate1 = readFileSync(
+  //     "/etc/letsencrypt/live/" + config.redirectUrl + "/cert.pem",
+  //     "utf8"
+  //   );
+  //   const ca1 = readFileSync(
+  //     "/etc/letsencrypt/live/" + config.redirectUrl + "/chain.pem",
+  //     "utf8"
+  //   );
+  //   const credentials1 = {
+  //     key: privateKey1,
+  //     cert: certificate1,
+  //     ca: ca1,
+  //   };
+  //   console.log("Ending load of certs");
+  //   // Certificate for Domain 3
+  //   // ... (just like line 22-29)
+  //   // Certificate for Domain 4
+  //   // ... (just like line 22-29)
+  //   // Starting both http & https servers
+  //   //const httpServer = http.createServer(app);
+  //   const httpsServer = https.createServer(credentials1, app);
+  //   console.log("Starting server");
+  //   app.listen(443, () => {
+  //     console.log("HTTPS Server running on port 443");
+  //   });
 };
