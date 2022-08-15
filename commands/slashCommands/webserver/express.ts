4a7a58819491ac32dc3e2ce6a4cfc4d9aa77b904
harrisonvanderbyl <harrison.vanderbyl@gmail.com>
2022-08-16 01:35:18 +1000

    remove http

--- commands/slashCommands/webserver/express.ts
+++ commands/slashCommands/webserver/express.ts
@@ -42,12 +42,12 @@ export const start = async () => {
   // ... (just like line 22-29)
 
   // Starting both http & https servers
-  const httpServer = http.createServer(app);
+  //const httpServer = http.createServer(app);
   const httpsServer = https.createServer(credentials1, app);
 
-  httpServer.listen(80, () => {
-    console.log("HTTP Server running on port 80");
-  });
+  //   httpServer.listen(80, () => {
+  //     console.log("HTTP Server running on port 80");
+  //   });
   httpsServer.listen(443, () => {
     console.log("HTTPS Server running on port 443");
   });
