1244f7fb0560e9202bed460b7841cf96c7ac8da8
harrisonvanderbyl <harrison.vanderbyl@gmail.com>
2022-08-25 22:42:24 +1000

    fix

--- commands/slashCommands/webserver/express.ts
+++ commands/slashCommands/webserver/express.ts
@@ -18,24 +18,24 @@ console.log("Starting load of server");
 console.log("Exporting starts");
 
 export const start = async () => {
-  // const privateKey1 = readFileSync(
-  //   "/etc/letsencrypt/live/" + config.redirectUrl + "/privkey.pem",
-  //   "utf8"
-  // );
-  // console.log(privateKey1);
-  // const certificate1 = readFileSync(
-  //   "/etc/letsencrypt/live/" + config.redirectUrl + "/cert.pem",
-  //   "utf8"
-  // );
-  // const ca1 = readFileSync(
-  //   "/etc/letsencrypt/live/" + config.redirectUrl + "/chain.pem",
-  //   "utf8"
-  // );
-  // const credentials1 = {
-  //   key: privateKey1,
-  //   cert: certificate1,
-  //   ca: ca1,
-  // };
+  const privateKey1 = readFileSync(
+    "/etc/letsencrypt/live/" + config.redirectUrl + "/privkey.pem",
+    "utf8"
+  );
+  console.log(privateKey1);
+  const certificate1 = readFileSync(
+    "/etc/letsencrypt/live/" + config.redirectUrl + "/cert.pem",
+    "utf8"
+  );
+  const ca1 = readFileSync(
+    "/etc/letsencrypt/live/" + config.redirectUrl + "/chain.pem",
+    "utf8"
+  );
+  const credentials1 = {
+    key: privateKey1,
+    cert: certificate1,
+    ca: ca1,
+  };
   console.log("Ending load of certs");
   // Certificate for Domain 3
   // ... (just like line 22-29)
@@ -43,9 +43,9 @@ export const start = async () => {
   // ... (just like line 22-29)
   // Starting both http & https servers
   //const httpServer = http.createServer(app);
-  //   const httpsServer = createServer(credentials1, app);
-  //   console.log("Starting server");
-  //   httpsServer.listen(443, () => {
-  //     console.log("HTTPS Server running on port 443");
-  //   });
+  const httpsServer = createServer(credentials1, app);
+  console.log("Starting server");
+  httpsServer.listen(443, () => {
+    console.log("HTTPS Server running on port 443");
+  });
 };
