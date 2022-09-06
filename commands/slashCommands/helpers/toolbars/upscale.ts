75c11739327799a068c8bace7fa2fb2f83ecbfce
harrisonvanderbyl <harrison.vanderbyl@gmail.com>
2022-09-06 15:59:26 +1000

    upscale

--- commands/slashCommands/helpers/toolbars/upscale.ts
+++ commands/slashCommands/helpers/toolbars/upscale.ts
@@ -7,6 +7,7 @@ import {
 import { ToolBarItem } from "./common";
 import { addToolbar } from "../buttons";
 import { client } from "../../../client";
+import sharp from "sharp";
 import { upscale } from "../../sdhelpers/upscale";
 
 export const upscaleToolBar: ToolBarItem = {
@@ -14,6 +15,8 @@ export const upscaleToolBar: ToolBarItem = {
   id: "upscale",
   filter: (buffers: Buffer[]) => buffers.length == 1,
   createToolbars: async (buffers: Buffer[]) => {
+    const { width, height } = await sharp(buffers[0]).metadata();
+    const maxsize = Math.max(width, height);
     // Different behaviors for different number of buffers
     switch (buffers.length) {
       case 1:
@@ -26,14 +29,37 @@ export const upscaleToolBar: ToolBarItem = {
                 custom_id: "upscale2x",
                 style: "SECONDARY",
                 label: "2x",
+                max: 2048,
+              },
+              {
+                type: "BUTTON",
+
+                custom_id: "upscale4x",
+                style: "SECONDARY",
+                label: "4x",
+                max: 1024,
+              },
+              {
+                type: "BUTTON",
+
+                custom_id: "upscale8x",
+                style: "SECONDARY",
+                label: "8x",
+                max: 512,
               },
               {
                 type: "BUTTON",
                 customId: "home",
                 style: "SECONDARY",
                 label: "🏠",
+                max: 0,
               },
-            ],
+            ]
+              .filter((m) => m.max <= maxsize)
+              .map((m) => {
+                delete m.max;
+                return m;
+              }),
             type: "ACTION_ROW",
           },
         ];
@@ -46,6 +72,14 @@ export const upscaleToolBar: ToolBarItem = {
     if (number == "upscale2x") {
       await i.deferUpdate();
       const message = await upscale(client, i, "2");
+    }
+    if (number == "upscale4x") {
+      await i.deferUpdate();
+      const message = await upscale(client, i, "4");
+    }
+    if (number == "upscale8x") {
+      await i.deferUpdate();
+      const message = await upscale(client, i, "8");
     }
   },
 };
