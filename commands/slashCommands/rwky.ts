89d1045a8f6385257bb1bea4a3047ed5fe6b1a59
harrisonvanderbyl <harrison.vanderbyl@gmail.com>
2022-09-09 22:05:42 +1000

    regex

--- commands/slashCommands/rwky.ts
+++ commands/slashCommands/rwky.ts
@@ -5,9 +5,11 @@ export const rwkyCommand: SlashCommand = {
   slashCommand: async (client, interaction) => {
     const clean = (str: string) => {
       console.log(str);
-      if (!isNaN(Number(str))) {
-        return str;
-      } else return "0.9";
+      const cs = str.split(".");
+      return (cs[0].replace(/[^0-9]/, "") + "." + cs.slice(1).join("")).replace(
+        /[^0-9]/,
+        ""
+      );
     };
     rwky(
       interaction,
