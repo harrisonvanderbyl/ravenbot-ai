dbd0c04bcc0c5e4d4bad768e2e2a0a331debda28
harrison <harrison.vanderbyl@gmail.com>
2022-09-14 15:19:34 +1000

    fix

--- commands/slashCommands/sdhelpers/webui/webui.ts
+++ commands/slashCommands/sdhelpers/webui/webui.ts
@@ -3,7 +3,7 @@ import { app } from "../../webserver/express";
 import { compileNetworkStats } from "../sdhelpers";
 export const startWebUi = ()=>{
 app.get("/webui/",(req,res)=>{
-    res.send(readFileSync(__dirname+"/webui.html"));
+    res.send(readFileSync(__dirname+"/web.html"));
 })
 app.get("/webui/ping", (req, res) => {
     res.send(compileNetworkStats(req.query.job))
