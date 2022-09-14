9042497484ccd40299ebb092d823977558532228
harrison <harrison.vanderbyl@gmail.com>
2022-09-14 11:16:47 +1000

    full worb list

---
+++ commands/slashCommands/sdhelpers/autodream/createPrompt.ts
@@ -0,0 +1,36 @@
+
+export const randomArray = (arr: any[]) => {
+    return arr[Math.floor(Math.random() * arr.length)];
+  };
+import nouns from "./Wordlist-Nouns-All.json"
+import verbs from "./Wordlist-Verbs-All.json"
+import adjectives from "./Wordlist-Adjectives-All.json"
+import adverbs from "./Wordlist-Adverbs-All.json"
+const mediums = [
+    "photo",
+    "drawing",
+    "painting",
+    "sculpture",
+    "digital art",
+    "mixed media",
+    "performance art",
+    "installation",
+    "video",
+    "sound",
+    "text",
+]
+
+
+export const createDreamString = ()=>
+`a ${randomArray(mediums)} of ${randomArray(adjectives)} ${randomArray(nouns)} ${randomArray(verbs)} ${randomArray(adverbs)} 
+digital art by ${randomArray([
+    "wlop",
+    "thomas kinkade",
+    "ted nasmith",
+    "wes anderson",
+    "marc simonetti",
+    "jim burns",
+    "gustave dore",
+    "greg rutkowski",
+    "ansel adams",
+  ])}`;
--- commands/slashCommands/sdhelpers/sdhelpers.ts
+++ commands/slashCommands/sdhelpers/sdhelpers.ts
@@ -11,101 +11,9 @@ import {
 
 import { app } from "../webserver/express";
 import { client } from "../../client";
+import { createDreamString, randomArray } from "./autodream/createPrompt";
 
-const Sizes = ["small", "medium", "large"];
 
-const CreatureStyles = [
-  "tree",
-  "flower",
-  "fern",
-  "grass",
-  "mushroom",
-  "mammal",
-  "reptile",
-  "insect",
-  "fish",
-  "bird",
-  "amphibian",
-  "woman",
-  "man",
-  "canine",
-  "feline",
-  "thing",
-];
-
-const MovementStyles = [
-  "bipedal",
-  "quadrupedal",
-  "flying",
-  "aerial",
-  "aquatic",
-  "stationary",
-  "arboreal",
-  "burrowing",
-  "beautiful",
-  "handsome",
-  "magical",
-  "magical",
-  "mundane",
-  "pretty",
-  "ugly",
-  "old",
-  "young",
-];
-
-const Enviroments = [
-  "meadows",
-  "forest",
-  "desert",
-  "cave",
-  "field",
-  "mountain",
-  "oasis",
-  "river",
-  "swamp",
-  "city",
-  "house",
-  "mars",
-  "moon",
-  "home",
-  "lakeside",
-  "place",
-];
-
-const Elements = [
-  "fire",
-  "water",
-  "earth",
-  "air",
-  "magma",
-  "ice",
-  "light",
-  "dark",
-  "clockwork",
-  "mechanical",
-  "electric",
-  "poison",
-  "radiation",
-  "lightning",
-  "grassy",
-  "vivic",
-  "wooden",
-  "metal",
-  "stone",
-  "glass",
-  "crystal",
-  "gem",
-  "gold",
-  "silver",
-  "copper",
-  "bronze",
-  "steel",
-  "paper",
-  "plastic",
-  "etherial",
-  "demonic",
-  "undead",
-];
 
 const NoNodeError = async (updatemessaged: Message) => {
   const messageToEdit = await client.guilds
@@ -456,38 +364,8 @@ export const updateNetworkStats = async () => {
           "1018090757149691945"
         )) as TextChannel;
         const message = await c.send("Idly dreaming ...");
-        const randomArray = (arr: any[]) => {
-          return arr[Math.floor(Math.random() * arr.length)];
-        };
-        const dream = `a ${randomArray(MovementStyles)} ${randomArray(
-          CreatureStyles
-        )} ${randomArray([
-          "dancing",
-          "running",
-          "flying",
-          "sitting",
-          "standing",
-          "dreaming",
-          "swimming",
-          "crawling",
-          "skithering",
-        ])} ${randomArray(Elements)} ${randomArray(
-          Enviroments
-        )} in the ${randomArray([
-          "past",
-          "future",
-          "present",
-        ])} digital art ${randomArray([
-          "wlop",
-          "thomas kinkade",
-          "ted nasmith",
-          "wes anderson",
-          "marc simonetti",
-          "jim burns",
-          "gustave dore",
-          "greg rutkowski",
-          "ansel adams",
-        ])} ${randomArray(["render", "painting", "sculpture", "sketch"])}`;
+       
+        const dream = createDreamString()
         const seed = (Math.random() * 100000).toFixed(0);
         const buffers = await stable(
           { id: message.id, fetchReply: () => message } as any,
