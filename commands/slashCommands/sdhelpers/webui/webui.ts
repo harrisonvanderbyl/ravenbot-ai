1d41e10ed493fdfa33f1644279476b5eafc5c2ef
harrison <harrison.vanderbyl@gmail.com>
2022-09-14 15:22:16 +1000

    fx

--- commands/slashCommands/sdhelpers/webui/webui.ts
+++ commands/slashCommands/sdhelpers/webui/webui.ts
@@ -3,6 +3,8 @@ import { app } from "../../webserver/express";
 import { compileNetworkStats } from "../sdhelpers";
 export const startWebUi = ()=>{
 app.get("/webui/",(req,res)=>{
+    // set to html
+    res.setHeader("Content-Type","text/html");
     res.send(readFileSync(__dirname+"/web.html"));
 })
 app.get("/webui/ping", (req, res) => {
