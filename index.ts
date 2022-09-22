8806e05e367cd6b2d771acf1ae3f5c2f811f07e8
harrison <harrison.vanderbyl@gmail.com>
2022-09-22 17:17:35 +1000

    update display

--- index.ts
+++ index.ts
@@ -43,14 +43,14 @@ client.on("messageCreate", async (message) => {
     getGuildCommands(undefined).then((commands) => {
       message.channel.send({
         content: `Commands available globally, either slash or context menu commands:\n ${commands
-          .map((c) => `*${c.name}* - ${c.description}`)
+          .map((c) => `**${c.name}** - *${c.description}*`)
           .join("\n")}`,
       });
     });
     getGuildCommands(message.guildId).then((commands) => {
       message.channel.send({
         content: `Commands available only in this server, either slash or context menu commands:\n ${commands
-          .map((c) => `*${c.name}* - ${c.description}`)
+          .map((c) => `**${c.name}** - *${c.description}*`)
           .join("\n")}`,
       });
     });
@@ -58,11 +58,11 @@ client.on("messageCreate", async (message) => {
   for (var c of Object.values(commands)) {
     if (message.content.startsWith("!help " + c.commandSchema.name)) {
       message.channel.send({
-        content: `Help for command\n*${c.commandSchema.name}*: ${
+        content: `Help for command\n**${c.commandSchema.name}**: *${
           c.commandSchema.description
-        }\n${
+        }*\n${
           c.commandSchema?.options
-            ?.map((o) => `\t*${o.name}* - ${o.description}`)
+            ?.map((o) => `\t$**${o.name}** - *${o.description}*`)
             .join("\n") ?? ""
         }`,
       });
