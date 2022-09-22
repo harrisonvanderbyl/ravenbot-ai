1e03cf74b68f7db5db46094e06dedc3d8f094c96
harrison <harrison.vanderbyl@gmail.com>
2022-09-22 22:44:22 +1000

    auto patreon add

--- commands/slashCommands/patreonData.ts
+++ commands/slashCommands/patreonData.ts
@@ -7,8 +7,46 @@ import {
 
 import { CommandInteraction } from "discord.js";
 import { SlashCommand } from "./typing";
+import { client } from "../client";
 
 const generateUserStats = async (data) => {};
+
+const giveRole = async (userid) => {
+  const g = await client.guilds.fetch("989166996153323591");
+  const member = await g.members.fetch(userid);
+  if (!member) {
+    console.log("Member not found");
+    return;
+  }
+  await member.roles.add("1022485145627934771");
+};
+
+export const updatePatreonData = async () => {
+  try {
+    const rawPatreonConfig = JSON.parse(
+      readFileSync("./patreonConfig.json").toString()
+    );
+    const patreonInfo = rawPatreonConfig["438605535323881486"];
+
+    const data = await getPatreonData(
+      patreonInfo.token,
+      "/current_user/campaigns"
+    );
+    const camp = data.rawJson.data[0].id;
+
+    const mypatreons = await getPatreonDiscordIDs(patreonInfo.token, camp);
+    rawPatreonConfig["438605535323881486"].patreons = mypatreons;
+    await Promise.all(
+      mypatreons.map((p) => giveRole(p.social_connections.discord.user_id))
+    );
+    writeFileSync("./patreonConfig.json", JSON.stringify(rawPatreonConfig));
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+// update every 2 minutes
+setInterval(updatePatreonData, 1000 * 60 * 2);
 
 export const patreonCommand: SlashCommand = {
   skipDeferReply: true,
@@ -34,6 +72,7 @@ export const patreonCommand: SlashCommand = {
 
       const mypatreons = await getPatreonDiscordIDs(patreonInfo.token, camp);
       rawPatreonConfig[interaction.user.id].patreons = mypatreons;
+
       writeFileSync("./patreonConfig.json", JSON.stringify(rawPatreonConfig));
 
       await interaction.editReply({
